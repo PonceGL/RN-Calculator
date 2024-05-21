2535bbba9cf9ab4a56805846b7bb0bdaf617edf8
“PonceGL” <poncianogl@hotmail.com>
2024-05-21 17:43:03 -0600

    feat(develop): eslint setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,65 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['@typescript-eslint', 'simple-import-sort'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: [
+    '@react-native-community',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  rules: {
+    '@typescript-eslint/no-var-requires': 0,
+    '@typescript-eslint/no-explicit-any': 1,
+    'react-native/no-unused-styles': 0,
+    'eslint-comments/no-unused-disable': 'off',
+    'react-native/no-inline-styles': 2,
+    'react-native/no-color-literals': 1,
+    'react-native/no-raw-text': 0,
+    'react-native/no-single-element-style-arrays': 2,
+    'react-hooks/exhaustive-deps': 0,
+    'no-console': [
+      'error',
+      {
+        allow: ['warn', 'error'],
+      },
+    ],
+    '@typescript-eslint/no-empty-function': 0,
+    'react/prop-types': 0,
+    'prettier/prettier': [
+      'error',
+      {
+        quoteProps: 'consistent',
+        singleQuote: true,
+        tabWidth: 2,
+        trailingComma: 'es5',
+        useTabs: false,
+      },
+    ],
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          ['^\\u0000'],
+          ['^'],
+          ['^react', '^@?\\w'],
+          ['^\\.'],
+          ['^node:'],
+        ],
+      },
+    ],
+  },
 };
--- App.tsx
+++ App.tsx
@@ -5,8 +5,8 @@
  * @format
  */
 
+import type { PropsWithChildren } from 'react';
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -16,7 +16,6 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
-
 import {
   Colors,
   DebugInstructions,
@@ -29,7 +28,7 @@ type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
+function Section({ children, title }: SectionProps): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
