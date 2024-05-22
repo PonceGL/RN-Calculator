574c24ff4516cafba14f8d26b2f01ca4d7c7f1f8
“PonceGL” <poncianogl@hotmail.com>
2024-05-22 14:04:22 -0600

    test(hooks): useColorTheme and useButtonWidth

---
+++ __tests__/src/hooks/useButtonWidth.test.ts
@@ -0,0 +1,18 @@
+import { Dimensions } from 'react-native';
+import { renderHook } from '@testing-library/react-hooks';
+
+import { useButtonWidth } from '../../../src/hooks/useButtonWidth';
+
+describe('useButtonWidth', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should return buttonWidth', () => {
+    jest
+      .spyOn(Dimensions, 'get')
+      .mockReturnValue({ width: 360, height: 818, scale: 2, fontScale: 2 });
+    const { result } = renderHook(() => useButtonWidth());
+    expect(result.current.buttonWidth).toBe(87.8048780487805);
+  });
+});
